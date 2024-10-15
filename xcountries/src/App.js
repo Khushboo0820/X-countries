51a105b19954ea7b1156ab7845fb06d9f76f9709
unknown <khushboo.parate74@gmail.com>
2024-10-15 18:48:36 +0530

    Countries added

--- xcountries/src/App.js
+++ xcountries/src/App.js
@@ -1,44 +1,33 @@
-import {useEffect,useState} from "react";
+import { useEffect, useState } from 'react';
 import './App.css';
-import Countries from "./components/Countries";
-import axios from "axios";
-
+import Countries from './components/Countries';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null); // For handling errors
-
-  useEffect(() => {
-    const getCountries = async () => {
-      try {
-        const response = await axios.get("https://xcountries-backend.azurewebsites.net/all");
-        setData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log("Error fetching data: ",error);
-        setError("Failed to fetch countries data.");
-        setLoading(false);
+  const [data,setData] = useState([]);
+  
+  useEffect(()=>{
+    const getCountries = async()=>{
+      try{
+        const resp = await axios.get("https://xcountries-backend.azurewebsites.net/all");
+        setData(resp.data);
+      }catch(err){
+        console.error("Error fetching data: ",err);
       }
-    };
-
+    }
     getCountries();
-  }, []);
-
+  },[]);
   return (
-    <div>
-      {loading ? (
-        <p>Loading...</p> // Display loading text
-      ) : (
-        <div className="wrapper">
-          {data.map((country) => (
-            <div key={`${country.name}-${country.abbr}`}>
-              <Countries name={country.name} flag={country.flag} abbr={country.abbr} />
-            </div>
-          ))}
-        </div>
-      )}
+    <div className='wrapper'>
+      {data.map((country)=>{
+        return(
+          <div key={`${country.name}-${country.abbr}`}>
+            <Countries name={country.name} flag={country.flag} abbr={country.abbr}/>
+          </div>
+        )
+      })}
     </div>
   );
 }
+
 export default App;
